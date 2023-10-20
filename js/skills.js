518a4a9bcebf9315a2e6bf2fbf22d756f00ae930
Allset <133010630+DeveloperFlow@users.noreply.github.com>
2023-10-21 00:01:04 +0100

    Delete js/skills.js

--- js/skills.js
+++
@@ -1,8 +0,0 @@
-/*skills catalogue*/
-var Skills = [
-    {name:"Web-Development",icon:"img/dev.png"},
-    {name:"Web-Design",icon:"img/design.png"},
-    {name:"Back-End Development",icon:"img/back-end.png"},
-    {name:"Front-End Development",icon:"img/front-end.png"},
-    {name:"Search Engine Optimization (S.E.O)",icon:"img/seo.png"}
-]
